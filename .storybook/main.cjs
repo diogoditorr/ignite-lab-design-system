6bbdc6d7702bd12891aade7a15de0a934480ae81
diogoditorr <35296262+diogoditorr@users.noreply.github.com>
2022-10-25 22:59:33 -0300

    ci: change vite base url

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,11 +15,11 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  // viteFinal: (config, { configType }) => {
-  //   if (configType === 'PRODUCTION') {
-  //     config.base = '/ignite-lab-design-system/'
-  //   }
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-lab-design-system/'
+    }
 
-  //   return config
-  // }
+    return config
+  }
 }
